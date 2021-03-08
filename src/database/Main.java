66a11ef73d8a587dbea9fbf2543eb6501083a8fa
Thomas Dean <tommy@dean321.com>
2021-03-08 17:49:17 -0500

    Create empty classes

--- src/database/Main.java
+++ src/database/Main.java
@@ -1,19 +1,11 @@
 package database;
 
 import database.records.InsertRecords;
-<<<<<<< HEAD
+
 import database.records.RetrieveRecords;
 import database.records.RetrieveSlideRecords;
 import database.records.RetrieveTextRecords;
 import database.tables.TableCreator;
-import database.tables.TableDropper;
-import javafx.scene.control.Tab;
-import javafx.scene.control.skin.SliderSkin;
-=======
-import database.records.RetrieveSlideRecords;
-import database.records.RetrieveTextRecords;
-import database.tables.TableCreator;
->>>>>>> c8ca8eb... Create empty classes
 
 import java.util.LinkedHashMap;
 
@@ -45,11 +37,8 @@ public class Main {
         TableCreator.create(url, "Slide", slideFields, foreignKeys);
 
         // Add records to the tables
-<<<<<<< HEAD
         InsertRecords.insertText(url,"Hello World", 100, 400, "Times New Roman", 11.5, "#000000");
-=======
         InsertRecords.insertText(url,"\u0989", 100, 400, "Times New Roman", 11.5, "#000000");
->>>>>>> c8ca8eb... Create empty classes
         InsertRecords.insertText(url, "Hello World", 200, 200, "Helvetica", 12, "#66ccff");
 
         InsertRecords.insertSlide(url, 1, 12);
