ec545ef7b4bdd5b150bc34bfe8d845227adeaa36
Thomas Dean <tommy@dean321.com>
2021-03-11 15:25:12 -0500

    Started creating the slideshow objects.

---
+++ src/slides/Present.java
@@ -0,0 +1,21 @@
+package slides;
+
+public class Present {
+    // TODO: Make a slideshow object to control from this class
+
+    public Present() {
+        // TODO: Make constructor for Present class
+    }
+    public void goForward() {
+        // TODO: Make the slideshow progress one slide forward
+    }
+    public void goBack() {
+        // TODO: Make the slideshow go one slide backward
+    }
+    public void toSlide(int slideNumber) {
+        // TODO: Make the slideshow go to the selected slide number
+    }
+    public void display() {
+        // TODO: Display the current slide
+    }
+}
